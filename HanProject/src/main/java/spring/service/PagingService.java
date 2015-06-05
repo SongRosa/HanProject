1557a6af7b0796ea182ad9c261fb3926579b404e
byhunting <byhunting@naver.com>
2015-06-05 17:53:24 +0900

    06/05 HJ

--- HanProject/src/main/java/spring/service/PagingService.java
+++ HanProject/src/main/java/spring/service/PagingService.java
@@ -51,7 +51,7 @@ public class PagingService {
 		// 이전 block 페이지
 		pagingHtml = new StringBuffer();
 		if (currentPage > blockPage) {
-			pagingHtml.append("<a href=/TestHan/board/list.do?p="
+			pagingHtml.append("<a href=/HanProject/board/list.do?p="
 					+ (startPage - 1) + ">");
 			pagingHtml.append("이전");
 			pagingHtml.append("</a>");
@@ -70,7 +70,7 @@ public class PagingService {
 				pagingHtml.append("</font></b>");
 			} else {
 				pagingHtml
-						.append("&nbsp;<a href='/TestHan/board/list.do?p=");
+						.append("&nbsp;<a href='/HanProject/board/list.do?p=");
 				pagingHtml.append(i);
 				pagingHtml.append("'>");
 				pagingHtml.append(i);
@@ -84,7 +84,7 @@ public class PagingService {
 
 		// 다음 block 페이지
 		if (totalPage - startPage >= blockPage) {
-			pagingHtml.append("<a href=/TestHan/board/list.do?p="
+			pagingHtml.append("<a href=/HanProject/board/list.do?p="
 					+ (endPage + 1) + ">");
 			pagingHtml.append("다음");
 			pagingHtml.append("</a>");
